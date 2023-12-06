befeac7d436538a529c730f07cc9dd585f19124d
noasakurajin <dev@noasakurajin.de>
2023-12-06 12:21:49 +0100

    adding gdb config and use gstreamer backend

--- src/main.cpp
+++ src/main.cpp
@@ -7,76 +7,75 @@ using namespace libtrainsim::core;
 
 class mainApp;
 
-const std::string appName = "thm.bahn_simulator.reference";
+const std::string               appName     = "thm.bahn_simulator.reference";
 static std::shared_ptr<mainApp> appInstance = nullptr;
 
-class mainApp : public SimpleGFX::SimpleGL::appLauncher{
-    private:
-        std::shared_ptr<libtrainsim::core::simulatorConfiguration> conf;
-        std::unique_ptr<mainWindow> menu;
-        std::shared_ptr<SimpleGFX::SimpleGL::loggerWindow> loggerWin;
+class mainApp : public SimpleGFX::SimpleGL::appLauncher {
+  private:
+    std::shared_ptr<libtrainsim::core::simulatorConfiguration> conf;
+    std::unique_ptr<mainWindow>                                menu;
+    std::shared_ptr<SimpleGFX::SimpleGL::loggerWindow>         loggerWin;
 
-        void load() override{
-            try{
-                //loggerWin = SimpleGFX::SimpleGL::loggerWindow::createManaged(SimpleGFX::loggingLevel::detail);
-                //conf->getLogger()->addExtraLogger(loggerWin);
-                //add_window(*loggerWin);
-                //loggerWin->set_visible(true);
-            }catch(...){
-                std::throw_with_nested(std::runtime_error("could not create logger window"));
-            }
-
-            try{
-                menu = std::make_unique<mainWindow>(conf, appInstance);
-                add_window(*menu);
-                menu->set_visible(true);
-                menu->present();
-                menu->registerWithEventManager(conf->getInputManager().get(), 0);
-            }catch(...){
-                conf->getLogger()->logCurrrentException(true);
-                std::throw_with_nested(std::runtime_error("could not create main menu"));
-            }
+    void load() override {
+        try {
+            // loggerWin = SimpleGFX::SimpleGL::loggerWindow::createManaged(SimpleGFX::loggingLevel::detail);
+            // conf->getLogger()->addExtraLogger(loggerWin);
+            // add_window(*loggerWin);
+            // loggerWin->set_visible(true);
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error("could not create logger window"));
         }
 
-        void on_startup() override {
-            appLauncher::on_startup();
+        try {
+            menu = std::make_unique<mainWindow>(conf, appInstance);
+            add_window(*menu);
+            menu->set_visible(true);
+            menu->present();
+            menu->registerWithEventManager(conf->getInputManager().get(), 0);
+        } catch (...) {
+            conf->getLogger()->logCurrrentException(true);
+            std::throw_with_nested(std::runtime_error("could not create main menu"));
         }
-    public:
-        mainApp() : appLauncher(appName, Gio::Application::Flags::NONE, false){
-            try{
-                conf = std::make_shared<simulatorConfiguration>("data/production_data/simulator.json", true, appName, true);
-            }catch(...){
-                std::throw_with_nested(std::runtime_error("could not load configuration"));
-            }
+    }
 
-            auto quit = Gio::SimpleAction::create("quit");
-            quit->signal_activate().connect([this](const Glib::VariantBase&){
-                this->quit();
-            });
-            add_action(quit);
+    void on_startup() override { appLauncher::on_startup(); }
 
-            auto menuBar = Gio::Menu::create();
-            auto quitItem = Gio::MenuItem::create("Quit", "app.quit");
-            menuBar->append_item(quitItem);
-
-            set_menubar(menuBar);
+  public:
+    mainApp()
+        : appLauncher(appName, Gio::Application::Flags::NONE, false) {
+        try {
+            conf = std::make_shared<simulatorConfiguration>("data/production_data/simulator.json", true, appName, true);
+        } catch (...) {
+            std::throw_with_nested(std::runtime_error("could not load configuration"));
         }
+
+        auto quit = Gio::SimpleAction::create("quit");
+        quit->signal_activate().connect([this](const Glib::VariantBase&) { this->quit(); });
+        add_action(quit);
+
+        auto menuBar  = Gio::Menu::create();
+        auto quitItem = Gio::MenuItem::create("Quit", "app.quit");
+        menuBar->append_item(quitItem);
+
+        set_menubar(menuBar);
+    }
 };
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 
 #ifdef GL_LINUX_DEBUG
-    setenv( "MESA_DEBUG", "context,incomplete_tex,incomplete_fbo", 1 );
-    setenv( "LIBGL_DEBUG", "verbose", 1 );
-    setenv( "RADV_DEBUG", "img,info,shaders", 1 );
-    setenv( "AMD_DEBUG", "info", 1 );
-    setenv( "mesa_glthread", "true", 1 );
-    //setenv("GDK_BACKEND", "x11", 1);
+    //setenv("MESA_DEBUG", "context,incomplete_tex,incomplete_fbo", 1);
+    //setenv("LIBGL_DEBUG", "verbose", 1);
+    //setenv("RADV_DEBUG", "img,info,shaders", 1);
+    //setenv("AMD_DEBUG", "info", 1);
+    setenv("mesa_glthread", "true", 1);
+    setenv("GST_DEBUG", "2", 1);
+    // setenv("GDK_BACKEND", "x11", 1);
 #endif
 
-    try{
+    try {
         appInstance = std::make_shared<mainApp>();
-    }catch(const std::exception& e){
+    } catch (const std::exception& e) {
         SimpleGFX::exception::printException(e);
         return 100;
     }
@@ -87,7 +86,7 @@ int main(int argc, char* argv[]){
 
 /*
 int main(int argc, char* argv[]){
-    
+
     //print the individual cmd arguments to make debugging them easier
     std::cout << "command line args:" << std::endl;
     for (int i = 0; i < argc;i++){
@@ -110,5 +109,5 @@ int main(int argc, char* argv[]){
     if(conf == nullptr){
         return 100;
     }
-    
+
 }*/
